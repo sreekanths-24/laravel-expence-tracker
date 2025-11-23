c08a195914a51ef0a2f8cef3bdbf56d0f66f5534
Sreekanth S <sreekanthsanthosh17@gmail.com>
2025-11-23 17:42:09 +0530

    created and migrated accounts table

---
+++ personalfinancetracker/database/migrations/2025_11_23_110651_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name', 150);
+            $table->enum('type', ['bank', 'card', 'cash', 'wallet', 'other'])->default('other');
+            $table->char('currency', 3)->default('INR');
+            $table->bigInteger('balance_cents')->default(0);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
