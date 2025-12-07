d2ec433e8006564154c9a058a7e5d60eda0a65f0
Sreekanth S <sreekanthsanthosh17@gmail.com>
2025-12-07 20:02:01 +0530

    migrated tags and transaction tags models

---
+++ personalfinancetracker/database/migrations/2025_12_07_142340_create_transaction_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('transaction_id');
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade');        
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade'); 
+            $table->index('transaction_id');
+            $table->index('tag_id');       
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_tags');
+    }
+};
